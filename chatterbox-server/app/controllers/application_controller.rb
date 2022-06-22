f5173a02c76db458c29e14d97cbac6fd3b011d3c
Aya Ramirez <ayaramz@gmail.com>
2022-06-21 20:22:09 -0400

    commit tree 75a9d410530d95d4be0af20aa312de50231a89f8

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < Sinatra::Base
+  # register Sinatra::ActiveRecordExtension
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    Message.all.order(:created_at, :desc).to_json
+  end
   
+  post '/messages' do
+    Message.create(body: params[:body], username: params[:username], created_at: params[:created_at], updated_at: params[:updated_at]).to_json
+  end
+  
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(body: params[:body], updated_at: Time.now)
+    message.to_json
+
+  end
+  
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    status 204
+  end
+
 end
